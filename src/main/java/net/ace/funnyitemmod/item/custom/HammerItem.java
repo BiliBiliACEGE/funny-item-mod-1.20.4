0580b4e0daa62ebc159e4a032248d409ee6f8e28
Ace哥 <3486598566@qq.com>
2024-05-20 15:01:17 +0800

    update

--- src/main/java/net/ace/funnyitemmod/item/custom/HammerItem.java
+++ src/main/java/net/ace/funnyitemmod/item/custom/HammerItem.java
@@ -1,19 +1,13 @@
 package net.ace.funnyitemmod.item.custom;
 
-import com.mojang.brigadier.CommandDispatcher;
-import com.mojang.brigadier.ParseResults;
-import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.ace.funnyitemmod.block.ModBlocks;
-import net.ace.funnyitemmod.block.custom.RuneAnvilBlock;
+import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
-import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
@@ -30,9 +24,10 @@ public class HammerItem extends Item {
         World world = context.getWorld();
         BlockPos pos = context.getBlockPos();
         PlayerEntity player = context.getPlayer();
+        Block block = world.getBlockState(pos).getBlock();
 
-        // 如果右键点击的是原版铁砧
-        if (world.getBlockState(pos).isOf(Blocks.ANVIL)) {
+        // 如果右键点击的是铁砧（包括所有损坏程度的铁砧）
+        if (block == Blocks.ANVIL || block == Blocks.CHIPPED_ANVIL || block == Blocks.DAMAGED_ANVIL) {
             // 播放铁砧的声音效果
             world.playSound(player, pos, SoundEvents.BLOCK_ANVIL_USE, SoundCategory.BLOCKS, 1.0f, 1.0f);
 
@@ -44,5 +39,4 @@ public class HammerItem extends Item {
 
         return ActionResult.PASS;
     }
-
 }
